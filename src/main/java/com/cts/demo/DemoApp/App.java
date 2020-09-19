4041582f618a92af90c772e134e616c26618e735
Shweta Pathak <shwetarawal2@gmail.com>
2020-09-19 11:27:41 +0530

    Initial commit

---
+++ src/main/java/com/cts/demo/DemoApp/App.java
@@ -0,0 +1,14 @@
+package com.cts.demo.DemoApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Hello.m1();
+    }
+}
---
+++ src/main/java/com/cts/demo/DemoApp/Hello.java
@@ -0,0 +1,7 @@
+package com.cts.demo.DemoApp;
+
+public class Hello {
+	public static void m1(){
+		System.out.println("m1");
+	}
+}
